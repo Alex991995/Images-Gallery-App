9d3af18e2a08926cdbcf307690da8dba89333324
Alex991995 <51911663+Alex991995@users.noreply.github.com>
2023-11-10 22:30:07 +0300

    add: fn signOut

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,7 +1,7 @@
 "use client"
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { useSession } from "next-auth/react"
+import { signOut, useSession } from "next-auth/react"
 
 type Props = {
   navLinks: NavLink[]
@@ -27,7 +27,7 @@ function Navigation({navLinks}:Props) {
         )
       })}
       {session.data?.user && <Link href="/profile">Profile</Link>}
-      {session.data?.user ? <Link href="/api/auth/signout" className="text-orange-200 hover:text-orange-300">Sign out</Link> :
+      {session.data?.user ? <Link href="#" onClick={() => signOut({callbackUrl: "/"})} className="text-orange-200 hover:text-orange-300">Sign out</Link> :
       <Link href="/api/auth/signin" className="text-orange-200 hover:text-orange-300">Sign in</Link>} 
     </>
   );
