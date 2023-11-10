0e02840581e2303bd21a2354aa078ac7c73504d5
Alex991995 <51911663+Alex991995@users.noreply.github.com>
2023-11-10 19:03:51 +0300

    add: cookies

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,16 @@
 export { default } from 'next-auth/middleware';
 
+import { cookies } from "next/headers"
+import { NextResponse } from "next/server";
+
+export const middleware = ( ) => {
+
+  const authCookie = cookies().get('next-auth.session-token');
+
+  if ( !authCookie || !authCookie.value ){
+    return NextResponse.redirect(process.env.NEXTAUTH_URL!);
+  }
+
+}
+
 export const config =  {matcher: ['/profile', '/protected/:path*'] }
